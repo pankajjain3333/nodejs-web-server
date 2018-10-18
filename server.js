81e18b30a698d6cc9e99e5c88b6857313092fb6e
Pankaj jain <pankaj.jain3333.pj@gmail.com>
2018-10-18 16:27:02 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
 var app=express();
+var port=process.env.PORT || 3000;
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getDate',()=>{
     return new Date().getFullYear();
@@ -10,16 +11,16 @@ hbs.registerHelper('capitalText',(text)=>{
     return text.toUpperCase();
 });
 app.set('view engine','hbs');
-// app.use((req,res,next)=>{
-//     var now =new Date().toString();
-//     var log=`${now}:${req.method} ${req.url}`;
-//     fs.appendFileSync('server.log',log +'\n');
-//     next();
-// });
-
 app.use((req,res,next)=>{
-    res.render('maintenance.hbs')
+    var now =new Date().toString();
+    var log=`${now}:${req.method} ${req.url}`;
+    fs.appendFileSync('server.log',log +'\n');
+    next();
 });
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs')
+// });
 app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
     res.render('home.hbs',{
@@ -38,6 +39,6 @@ app.get('/bad',(req,res)=>{
         Error:'something went wrong'
     });
 });
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
 });
